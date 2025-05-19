df264adeb3c1a34690a37d40ebb5dca83633f878
thibault <thibault@thvi.ch>
2025-05-19 20:48:44 +0200

    remove ctrl keys

--- lua/bepo.lua
+++ lua/bepo.lua
@@ -26,34 +26,3 @@ vim.opt.langmap = vim.fn.join({
     escape(be_lowr) .. ';' .. escape(qw_lowr),
     escape(be_uppr) .. ';' .. escape(qw_uppr),
 }, ',')
-
--- Control key mapping
-local ascii_letter = '[A-Za-z]'
-
----@param be string  -- Bépo string
----@param qw string  -- Qwerty string (same length)
-local function map_ctrl(be, qw)
-  -- split each string into an array of UTF-8 characters
-  local be_chars = vim.fn.split(be, '\\zs')
-  local qw_chars = vim.fn.split(qw, '\\zs')
-
-  -- if the two tables don’t have exactly the same number of chars,
-  -- something is wrong with our source data – abort loudly
-  assert(#be_chars == #qw_chars,
-         'Bépo/Qwerty tables differ: ' .. #be_chars .. ' vs ' .. #qw_chars)
-
-  for i = 1, #be_chars do
-    local b = be_chars[i]
-    local q = qw_chars[i]
-    -- only plain ASCII letters can form a Ctrl-<char> sequence
-    if b:match(ascii_letter) and q:match(ascii_letter) then
-      local lhs = '<C-' .. b .. '>'
-      local rhs = '<C-' .. q .. '>'
-      vim.keymap.set({'n','v','o','s','i','c','t'}, lhs, rhs,
-                     { noremap = true, silent = true,
-                       desc = 'Bépo-Ctrl → Qwerty-Ctrl' })
-    end
-  end
-end
-
-map_ctrl(be_lowr, qw_lowr)
